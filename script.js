f8a91076ffd5b0c855cbad3ed5aecd8c228468fd
ostapbondarenko <120116710+ostapbondarenko@users.noreply.github.com>
2023-05-04 15:19:36 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,18 +1,26 @@
+document.getElementById('weatherButton').addEventListener('click', getWeather);
+
 function getWeather() {
-  const apiKey = "59d8de3e36432775b2494c3629dfb691";
-  const city = "London";
-  const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`;
+  var cityName = 'Лондон';
+  var apiKey = 'cec9cd8db9ad7af9992cbf7623bcf472'; 
+
+  var apiUrl = 'https://api.openweathermap.org/data/2.5/weather?q=' + cityName + '&appid=' + apiKey;
 
   fetch(apiUrl)
-    .then(response => response.json())
-    .then(data => {
-      const temperature = data.main.temp - 273.15;
-      const city = data.name;
-      document.getElementById("weatherData").innerText = `Температура в місті ${city} становить ${temperature.toFixed(2)} градусів Цельсія`;
+    .then(function(response) {
+      return response.json();
     })
-    .catch(error => {
-      console.log("Сталася помилка під час отримання даних про погоду:", error);
+    .then(function(data) {
+      var temperature = data.main.temp;
+      var humidity = data.main.humidity;
+      var windSpeed = data.wind.speed;
+
+      var weatherDataDiv = document.getElementById('weatherData');
+      weatherDataDiv.innerHTML = 'Температура: ' + temperature + 'K<br>' +
+                                 'Вологість: ' + humidity + '%<br>' +
+                                 'Швидкість вітру: ' + windSpeed + ' м/с';
+    })
+    .catch(function(error) {
+      console.log('Виникла помилка при отриманні даних про погоду:', error);
     });
 }
-
-document.getElementById("weatherButton").addEventListener("click", getWeather);
