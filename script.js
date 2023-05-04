99522d5b4d1a7c3204c0d79670c0a97574fd8c83
ostapbondarenko <120116710+ostapbondarenko@users.noreply.github.com>
2023-05-04 15:13:25 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,19 +1,13 @@
-// Функція, яка виконується при натисканні кнопки
 function getWeather() {
-  // URL запиту до API OpenWeatherMap
   const apiKey = "YOUR_API_KEY";
   const city = "London";
   const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`;
 
-  // Виконуємо запит до API
   fetch(apiUrl)
     .then(response => response.json())
     .then(data => {
-      // Витягуємо температуру з відповіді
       const temperature = data.main.temp - 273.15;
-      // Витягуємо назву міста з відповіді
       const city = data.name;
-      // Відображаємо дані на веб-сторінці
       document.getElementById("weatherData").innerText = `Температура в місті ${city} становить ${temperature.toFixed(2)} градусів Цельсія`;
     })
     .catch(error => {
@@ -21,5 +15,4 @@ function getWeather() {
     });
 }
 
-// Прив'язуємо функцію до кнопки
 document.getElementById("weatherButton").addEventListener("click", getWeather);
